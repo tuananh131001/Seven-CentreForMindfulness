abe7c8338ce3ad216d08ec8110a5b166438b003a
Giang Nhật Khánh (Kayden) <nhatkhanhgiang2512@gmail.com>
2023-06-22 10:34:05 +0700

    implemented reflection card UI

--- App.js
+++ App.js
@@ -1,34 +1,29 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { AssessmentView } from "./src/pages/AssessmentView";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeBaseProvider } from "native-base";
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-import { HomeView } from './src/pages/HomeView';
+import * as React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { AssessmentView } from "./src/pages/AssessmentView"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NativeBaseProvider } from "native-base"
+import { store } from "./app/store"
+import { Provider } from "react-redux"
+import { HomeView } from "./src/pages/HomeView"
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 const AppNavigator = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="HomeView"
-        component={HomeView}
-      />
-    </Stack.Navigator>
-    
-  </NavigationContainer>
-);
+	<NavigationContainer>
+		<Stack.Navigator>
+			<Stack.Screen name="HomeView" component={HomeView} />
+		</Stack.Navigator>
+	</NavigationContainer>
+)
 
 const App = () => {
-  return (
-    <Provider store={store}>    
-      <NativeBaseProvider>
-        <AppNavigator />
-      </NativeBaseProvider>
-    </Provider>
+	return (
+		<Provider store={store}>
+			<NativeBaseProvider>
+				<AppNavigator />
+			</NativeBaseProvider>
+		</Provider>
+	)
+}
 
-  );
-};
-
-export default App;
+export default App
--- src/pages/HomeView.js
+++ src/pages/HomeView.js
@@ -1,20 +1,33 @@
-import { View, Text } from 'react-native';
-import { TwoColumnCard } from '../components/TwoColumnCard';
-import { Badge, Button, Center, Container, HStack, Heading, VStack } from 'native-base';
-import { TwoRowCard } from '../components/TwoRowCard';
+import { View, Text } from "react-native"
+import { TwoColumnCard } from "../components/TwoColumnCard"
+import {
+	Badge,
+	Button,
+	Center,
+	Container,
+	HStack,
+	Heading,
+	ScrollView,
+	VStack,
+} from "native-base"
+import { TwoRowCard } from "../components/TwoRowCard"
+import { ReflectionCard } from "../components/ReflectionCard"
 
 export const HomeView = () => {
-  return (
-      <VStack margin="5" space="5">
-        <Heading>
-          👋 Hi, Sir
-        </Heading>
-        <HStack space={5} >
-          <Button color="cyan.500" borderRadius="15">Audio</Button>
-          <Button colorScheme="secondary" borderRadius="15" >Video</Button>
-        </HStack>
-        <TwoColumnCard/>
-        <TwoRowCard/>
-      </VStack>
-  );
-};
+	return (
+		<VStack margin="5" space="5">
+			<Heading>👋 Hi, Sir</Heading>
+			<HStack space={5}>
+				<Button color="cyan.500" borderRadius="15">
+					Audio
+				</Button>
+				<Button colorScheme="secondary" borderRadius="15">
+					Video
+				</Button>
+			</HStack>
+			<ReflectionCard />
+			<TwoColumnCard />
+			<TwoRowCard />
+		</VStack>
+	)
+}
