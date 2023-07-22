f5b3ef373fae1787844f392f0eefd7ab12e12dc5
william <s3864077@rmit.edu.vn>
2023-07-22 20:47:13 +0700

    Switch to Dropbox

--- src/pages/AudioView.js
+++ src/pages/AudioView.js
@@ -1,40 +1,50 @@
 import { useState, useEffect, useRef } from 'react'
-import { Flex, View, IconButton, useToast } from 'native-base'
+import {
+  Flex,
+  View,
+  IconButton,
+  useToast,
+  VStack,
+  HStack,
+  Heading,
+  Text,
+  Slider,
+  Image,
+  Box,
+} from 'native-base'
 import { AppState } from 'react-native'
 import { Audio } from 'expo-av'
-import { FIREBASE_APP } from '../../firebaseConfig'
-import { primaryColor, secondaryColor } from '../../assets/ColorConst'
-import { AudioThumbnailCard } from '../components/AudioThumbnailCard'
-import { AudioControlsCard } from '../components/AudioControlsCard'
+import {
+  audioAccentColor,
+  audioPrimaryColor,
+  primaryColor,
+  secondaryColor,
+} from '../../assets/ColorConst'
 import { MaterialIcons } from '@expo/vector-icons'
 import { AudioNavigationCard } from '../components/AudioNavigationCard'
-import { getStorage, ref, getDownloadURL } from 'firebase/storage'
 import { AlertToast } from '../components/Toast'
 import { collection, addDoc } from 'firebase/firestore'
 import { FIREBASE_DB } from '../../firebaseConfig'
-import { use } from 'i18next'
 
 export const AudioView = ({ route, navigation }) => {
+  const { title, link } = route.params
+
   const [isPlayed, setIsPlayed] = useState(false)
   const [durationAudio, setDurationAudio] = useState(0)
   const [currentTimestamp, setCurrentTimestamp] = useState(0)
-  const { fileName, title } = route.params
   const [sound, setSound] = useState()
-  const toast = useToast()
   const [activeTime, setActiveTime] = useState(0)
   const [usageTimerRun, setUsageTimerRun] = useState(false)
-
+  const toast = useToast()
   const appState = useRef(AppState.currentState)
   const soundObject = new Audio.Sound()
 
   async function playSound() {
-    const storage = getStorage(FIREBASE_APP)
-    const url = await getDownloadURL(ref(storage, fileName))
     await Audio.setAudioModeAsync({ playsInSilentModeIOS: true })
     try {
-      await soundObject.loadAsync({ uri: url })
+      await soundObject.loadAsync({ uri: link })
       setSound(soundObject)
-      soundObject.getStatusAsync().then(function (result) {
+      soundObject.getStatusAsync().then((result) => {
         setDurationAudio(result.durationMillis)
       })
       await soundObject.playAsync()
@@ -43,12 +53,16 @@ export const AudioView = ({ route, navigation }) => {
     }
   }
 
-  const startUsageTimer = () => {
-    setUsageTimerRun(true)
+  const toggleAudioStatus = () => {
+    setIsPlayed(!isPlayed)
+    isPlayed ? sound.unloadAsync() : playSound()
+    isPlayed ? setUsageTimerRun(false) : setUsageTimerRun(true)
   }
 
-  const pauseUsageTimer = () => {
-    setUsageTimerRun(false)
+  function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000)
+    var seconds = ((millis % 60000) / 1000).toFixed(0)
+    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
   }
 
   const handleBackButtonClick = () => {
@@ -56,25 +70,18 @@ export const AudioView = ({ route, navigation }) => {
 
     if (sound !== undefined) {
       sound.unloadAsync()
-      pauseUsageTimer()
-      addUsageTimeData()
+      setUsageTimerRun(false)
+      const docRef = collection(FIREBASE_DB, 'usageTime')
+      addDoc(docRef, { usageTime: activeTime })
     }
-  }
-
-  const addUsageTimeData = async () => {
-    const docRef = collection(FIREBASE_DB, 'usageTime')
-
-    await addDoc(docRef, { usageTime: activeTime })
   }
 
   useEffect(() => {
-    var interval
+    if (usageTimerRun === false) return
 
-    if (usageTimerRun === true) {
-      interval = setInterval(() => {
-        setActiveTime((prev) => prev + 1)
-      }, 1000)
-    }
+    let interval = setInterval(() => {
+      setActiveTime((prev) => prev + 1)
+    }, 1000)
 
     return () => clearInterval(interval)
   }, [usageTimerRun])
@@ -90,10 +97,10 @@ export const AudioView = ({ route, navigation }) => {
     const subscription = AppState.addEventListener('change', (nextAppState) => {
       if (appState.current.match(/active/) && nextAppState === 'background') {
         sound.unloadAsync()
-        pauseUsageTimer()
+        setUsageTimerRun(false)
       } else if (appState.current.match(/background/) && nextAppState === 'active') {
         playSound()
-        startUsageTimer()
+        setUsageTimerRun(true)
       }
       appState.current = nextAppState
     })
@@ -129,18 +136,71 @@ export const AudioView = ({ route, navigation }) => {
         mt="auto"
         pt="10"
       >
-        <AudioThumbnailCard title={JSON.stringify(title).replaceAll('"', '')} />
-        <AudioControlsCard
-          sound={sound}
-          currentTimestamp={currentTimestamp}
-          duration={durationAudio}
-          isPlayed={isPlayed}
-          setIsPlayed={setIsPlayed}
-          playSound={playSound}
-          activeTime={activeTime}
-          startUsageTimer={startUsageTimer}
-          pauseUsageTimer={pauseUsageTimer}
-        />
+        <VStack>
+          <Box borderRadius="full" borderColor={audioPrimaryColor} borderWidth="4">
+            <Box borderRadius="full" bg={audioPrimaryColor} margin="3">
+              <Image
+                source={{
+                  uri: 'https://blush.design/api/download?shareUri=8rihonJGqaYdyiU_&c=Hair_0%7E0f0f0f_Skin_0%7Ef6cbc3&w=800&h=800&fm=png',
+                }}
+                alt="Self-reflection"
+                size="175"
+                margin="5"
+                resizeMode="contain"
+              />
+            </Box>
+          </Box>
+          <VStack margin="5" alignItems="center" space="2">
+            <Heading fontWeight="medium">{title}</Heading>
+            <Text fontWeight="thin">Inner Peace</Text>
+          </VStack>
+        </VStack>
+        <VStack space="5" alignItems="center">
+          <HStack space="5">
+            <IconButton
+              icon={<MaterialIcons name="fast-rewind" size={30} color={audioAccentColor} />}
+              variant="ghost"
+            />
+            <IconButton
+              icon={
+                <MaterialIcons
+                  name={isPlayed ? 'pause' : 'play-arrow'}
+                  size={60}
+                  onPress={toggleAudioStatus}
+                  color={secondaryColor}
+                />
+              }
+              variant="solid"
+              backgroundColor={audioPrimaryColor}
+              borderRadius="full"
+            />
+            <IconButton
+              icon={<MaterialIcons name="fast-forward" size={30} color={audioAccentColor} />}
+              variant="ghost"
+            />
+          </HStack>
+          <Flex direction="row" justifyContent="space-between" alignItems="center">
+            <Text>{millisToMinutesAndSeconds(currentTimestamp)}</Text>
+            <View>
+              <Slider
+                width="250"
+                size="md"
+                minValue={0}
+                value={currentTimestamp || 0}
+                maxValue={durationAudio}
+                defaultValue={0}
+                marginX={2.5}
+              >
+                <Slider.Track bg={audioAccentColor}>
+                  <Slider.FilledTrack bg={primaryColor} />
+                </Slider.Track>
+                <Slider.Thumb bg={primaryColor} />
+              </Slider>
+            </View>
+            <Text>{millisToMinutesAndSeconds(durationAudio)}</Text>
+          </Flex>
+          <Text>{activeTime}</Text>
+        </VStack>
       </Flex>
       <AudioNavigationCard />
     </View>
