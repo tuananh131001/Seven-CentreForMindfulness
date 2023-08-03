05cf35e329a4d77d72aee8ba05ac4f1ea27e7c59
Quan Le <quanmenden@gmail.com>
2023-08-03 21:47:47 +0700

    Add calculate assessment score to firebase

--- src/pages/AssessmentView.js
+++ src/pages/AssessmentView.js
@@ -16,9 +16,28 @@ const ICONNAMES = [
   'water-outline',
 ]
 
+const optionsToScore = {
+  "Deveplop Gratitude": 1,
+  "Better Sleep": 2,
+  "Improve Performance": 3,
+  "Reduce Stress": 4,
+  "Increase Happiness": 5,
+  "Build Self Esteem": 6,
+  "Reduce Anxiety": 7,
+};
+
 export const AssessmentView = ({ navigation }) => {
   const { t } = useTranslation()
   const { signedIn, dispatchSignedIn } = useContext(SignInContext)
+  const calculateScore = (selected) => {
+    let score = 0;
+    for (const str of selected) {
+      if (Object.prototype.hasOwnProperty.call(optionsToScore, str)) {
+        score += optionsToScore[str];
+      }
+    }
+    return score;
+  }
   const BUTTONTEXT = [
     t('DeveplopGratitude'),
     t('BetterSleep'),
@@ -31,8 +50,10 @@ export const AssessmentView = ({ navigation }) => {
   const [selected, setSelected] = useState([])
 
   const handleContinueButton = async () => {
+    const score = calculateScore(selected);
     if (selected.length > 0) {
       updateUserFields(signedIn.uid, { isCompletedTest: true })
+      updateUserFields(signedIn.uid, { assessmentScore: score })
       dispatchSignedIn({ type: 'SET_TEST_STATUS', payload: { isCompletedTest: true } })
       navigation.navigate('HomeStack')
     } else {
