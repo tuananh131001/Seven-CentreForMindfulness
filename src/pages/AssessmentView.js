b572a555757121b6c4b40436c05336e45f1d17a8
Quan Le <quanmenden@gmail.com>
2023-08-05 15:07:46 +0700

    Fix var

--- src/pages/AssessmentView.js
+++ src/pages/AssessmentView.js
@@ -16,44 +16,21 @@ const ICONNAMES = [
   'water-outline',
 ]
 
-const optionsToScore = {
-  "Deveplop Gratitude": 1,
-  "Better Sleep": 2,
-  "Improve Performance": 3,
-  "Reduce Stress": 4,
-  "Increase Happiness": 5,
-  "Build Self Esteem": 6,
-  "Reduce Anxiety": 7,
-};
+const OPTION_LIST = ['option1', 'option2', 'option3', 'option4', 'option5', 'option6', 'option7']
 
 export const AssessmentView = ({ navigation }) => {
   const { t } = useTranslation()
   const { signedIn, dispatchSignedIn } = useContext(SignInContext)
-  const calculateScore = (selected) => {
-    let score = 0;
-    for (const str of selected) {
-      if (Object.prototype.hasOwnProperty.call(optionsToScore, str)) {
-        score += optionsToScore[str];
-      }
-    }
-    return score;
-  }
-  const BUTTONTEXT = [
-    t('DeveplopGratitude'),
-    t('BetterSleep'),
-    t('ImprovePerformance'),
-    t('ReduceStress'),
-    t('IncreaseHappiness'),
-    t('BuildSelfEsteem'),
-    t('ReduceAnxiety'),
-  ]
   const [selected, setSelected] = useState([])
+  const BUTTONTEXT = OPTION_LIST.map((option) => t(option))
 
   const handleContinueButton = async () => {
-    const score = calculateScore(selected);
     if (selected.length > 0) {
-      updateUserFields(signedIn.uid, { isCompletedTest: true })
-      updateUserFields(signedIn.uid, { assessmentScore: score })
+      let score = selected.reduce(
+        (accumulator, currentValue) => accumulator + (currentValue + 1),
+        0,
+      )
+      updateUserFields(signedIn.uid, { isCompletedTest: true, assessmentScore: score })
       dispatchSignedIn({ type: 'SET_TEST_STATUS', payload: { isCompletedTest: true } })
       navigation.navigate('HomeStack')
     } else {
@@ -77,6 +54,7 @@ export const AssessmentView = ({ navigation }) => {
               <ChoiceButton
                 selected={selected}
                 setSelected={setSelected}
+                buttonIndex={index}
                 iconName={iconName}
                 buttonText={BUTTONTEXT[index]}
                 key={iconName}
