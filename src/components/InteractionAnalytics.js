d332a2698bfcb333d5622b22444a152b910f698d
Quan Le <quanmenden@gmail.com>
2023-08-12 11:22:48 +0700

    i18n for audio resource

--- src/components/InteractionAnalytics.js
+++ src/components/InteractionAnalytics.js
@@ -1,8 +1,10 @@
 import { VStack, HStack, Flex, Heading, Text } from 'native-base'
 
 import { progressPrimaryColor, progressSecondaryColor } from '../../assets/ColorConst'
+import { useTranslation } from 'react-i18next'
 
 export const InteractionAnalytics = () => {
+  const { t } = useTranslation();
   return (
     <VStack space="10" width="100%" my="5">
       <HStack space="5" width="100%" justifyContent="space-between">
@@ -14,7 +16,7 @@ export const InteractionAnalytics = () => {
           borderRadius="20"
         >
           <Heading>7d</Heading>
-          <Text color={progressSecondaryColor}>current streak</Text>
+          <Text color={progressSecondaryColor}>{t('CurrentStreak')}</Text>
         </Flex>
         <Flex
           bg={progressPrimaryColor}
@@ -24,7 +26,7 @@ export const InteractionAnalytics = () => {
           borderRadius="20"
         >
           <Heading>10d</Heading>
-          <Text color={progressSecondaryColor}>longest streak</Text>
+          <Text color={progressSecondaryColor}>{t('LongestStreak')}</Text>
         </Flex>
       </HStack>
       <HStack space="5" width="100%" justifyContent="space-between">
@@ -36,7 +38,7 @@ export const InteractionAnalytics = () => {
           borderRadius="20"
         >
           <Heading>15</Heading>
-          <Text color={progressSecondaryColor}>audio tracks completed</Text>
+          <Text color={progressSecondaryColor}>{t('AudioCompleted')}</Text>
         </Flex>
         <Flex
           bg={progressPrimaryColor}
@@ -46,7 +48,7 @@ export const InteractionAnalytics = () => {
           borderRadius="20"
         >
           <Heading>5</Heading>
-          <Text color={progressSecondaryColor}>activities completed</Text>
+          <Text color={progressSecondaryColor}>{t('ActivityCompleted')}</Text>
         </Flex>
       </HStack>
     </VStack>
