f8a93a7409007da081a965db081f508d4d791383
TM3 <laqu@netcompany.com>
2023-09-07 11:45:24 +0700

    Notification trans

--- src/components/NotificationCard.js
+++ src/components/NotificationCard.js
@@ -59,10 +59,10 @@ export const NotificationCard = () => {
       {scheduledNotifications.map((notification) => (
         <Box key={notification.identifier}>
           <Text>ID: {notification.identifier}</Text>
-          <Text>Body:{notification.content.body}</Text>
+          <Text>{t('Body')}:{notification.content.body}</Text>
           <Text>
-            Trigger at: {handleCrossPlatfromDate(notification.trigger).hour} hours{' '}
-            {handleCrossPlatfromDate(notification.trigger).minute} minutes
+            {t('Trigger')} {handleCrossPlatfromDate(notification.trigger).hour} {t('Hour')}{' '}
+            {handleCrossPlatfromDate(notification.trigger).minute} {t('Minute')}
           </Text>
         </Box>
       ))}
